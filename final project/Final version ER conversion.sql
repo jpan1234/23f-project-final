2c24543daf8ca48a85313250ff6c5040eca5a4fa
jpan123 <pan.je@northeastern.edu>
2023-12-04 00:29:40 -0500

    added not null parameter

--- final project/Final version ER conversion.sql
+++ final project/Final version ER conversion.sql
@@ -1,189 +1,192 @@
-# creating database HuskyHealth
+
+-- creating database HuskyHealth
 CREATE DATABASE IF NOT EXISTS HuskyHealth;
 
-# use the database
+-- use the database
 USE HuskyHealth;
 
-# creating the Patient table
+-- creating the Patient table
 CREATE TABLE IF NOT EXISTS Patient
 (
-    patientID INT AUTO_INCREMENT PRIMARY KEY,
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    birthDate DATE,
-    age INT
+    patientID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    birthDate DATE NOT NULL,
+    age INT,
+    healthRecordConsent TINYINT(1) DEFAULT 0 NOT NULL
 );
 
-# updating the age to be the timestamp difference between birth date and current date
+-- updating the age to be the timestamp difference between birth date and current date
 UPDATE Patient SET age = TIMESTAMPDIFF(YEAR, birthDate, CURDATE());
 
-# creating the Doctor table
+-- creating the Doctor table
 CREATE TABLE IF NOT EXISTS Doctor (
-    doctorID INT AUTO_INCREMENT PRIMARY KEY,
-    specialization VARCHAR(255),
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    consent TINYINT(1) DEFAULT 1
+    doctorID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    specialization VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    consent TINYINT(1) DEFAULT 1 NOT NULL
 );
 
-# creating WellnessCoach table
+-- creating WellnessCoach table
 CREATE TABLE IF NOT EXISTS WellnessCoach (
-    coachID INT AUTO_INCREMENT PRIMARY KEY,
-    education VARCHAR(255),
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    consent TINYINT(1) DEFAULT 0
+    coachID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    education VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    consent TINYINT(1) DEFAULT 0 NOT NULL
 );
 
-# creating InsuranceRepresentative table
+-- creating InsuranceRepresentative table
 CREATE TABLE IF NOT EXISTS InsuranceRepresentative (
-    repID INT AUTO_INCREMENT PRIMARY KEY,
-    company VARCHAR(255),
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    consent TINYINT(1) DEFAULT 0
+    repID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    company VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    consent TINYINT(1) DEFAULT 0 NOT NULL
 );
 
-# creating Visit table
+-- creating Visit table
 CREATE TABLE IF NOT EXISTS Visit (
-    visitID INT AUTO_INCREMENT PRIMARY KEY,
-    purpose VARCHAR(255),
-    visitDate DATE,
-    patientID INT,
+    visitID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    purpose VARCHAR(255) NOT NULL,
+    visitDate DATE NOT NULL,
+    patientID INT NOT NULL,
     doctorID INT,
     coachID INT,
-    canceled TINYINT(1) DEFAULT 0,
+    canceled TINYINT(1) DEFAULT 0 NOT NULL,
     FOREIGN KEY (patientID) REFERENCES Patient(patientID) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (doctorID) REFERENCES Doctor(doctorID) ON UPDATE CASCADE ON DELETE SET NULL,
     FOREIGN KEY (coachID) REFERENCES WellnessCoach(coachID) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
-# creating LabResults table
+-- creating LabResults table
 CREATE TABLE IF NOT EXISTS LabResults (
-    testID INT AUTO_INCREMENT PRIMARY KEY,
-    result TEXT,
-    type VARCHAR(255),
-    testDate DATE,
-    patientID INT,
-    FOREIGN KEY (patientID) REFERENCES Patient(patientID) ON UPDATE CASCADE ON DELETE SET NULL
+    testID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    result TEXT NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    testDate DATE NOT NULL,
+    patientID INT NOT NULL,
+    FOREIGN KEY (patientID) REFERENCES Patient(patientID) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-
-
-# creating Prescriptions table
+-- creating Prescriptions table
 CREATE TABLE IF NOT EXISTS Prescriptions (
-    scriptID INT AUTO_INCREMENT PRIMARY KEY,
-    type VARCHAR(255),
+    scriptID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    type VARCHAR(255) NOT NULL,
     visitID INT,
     testID INT,
-    patientID INT,
-    resultDate DATE,
-    company VARCHAR(255),
-    doctorID INT,
-    status VARCHAR(50),
-    datePrescribed DATE,
-    pharmacy VARCHAR(255),
-    medication VARCHAR(255),
-    duration INT,
-    inactive TINYINT(1) DEFAULT 0,
+    patientID INT NOT NULL,
+    resultDate DATE NOT NULL,
+    company VARCHAR(255) NOT NULL,
+    doctorID INT NOT NULL,,
+    status VARCHAR(50) NOT NULL,
+    datePrescribed DATE NOT NULL,
+    pharmacy VARCHAR(255) NOT NULL,
+    medication VARCHAR(255) NOT NULL,
+    duration INT NOT NULL,
+    inactive TINYINT(1) DEFAULT 0 NOT NULL,
     FOREIGN KEY (visitID) REFERENCES Visit(visitID) ON UPDATE CASCADE ON DELETE SET NULL,
     FOREIGN KEY (testID) REFERENCES LabResults(testID) ON UPDATE CASCADE ON DELETE SET NULL,
     FOREIGN KEY (patientID) REFERENCES Patient(patientID) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (doctorID) REFERENCES Doctor(doctorID) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
-# creating HealthRecords table
+-- creating HealthRecords table
 CREATE TABLE IF NOT EXISTS HealthRecords (
-    healthRecordID INT AUTO_INCREMENT PRIMARY KEY,
-    familyHistory TEXT,
-    allergies TEXT,
-    vaxHistory TEXT,
-    patientID INT,
+    healthRecordID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    familyHistory TEXT NOT NULL,
+    allergies TEXT NOT NULL,
+    vaxHistory TEXT NOT NULL,
+    patientID INT NOT NULL,
     doctorID INT,
     scriptID INT,
-    complete TINYINT(1) DEFAULT 0,
-    FOREIGN KEY (patientID) REFERENCES Patient(patientID) ON UPDATE CASCADE ON DELETE SET NULL,
+    complete TINYINT(1) DEFAULT 0 NOT NULL,
+    FOREIGN KEY (patientID) REFERENCES Patient(patientID) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (doctorID) REFERENCES Doctor(doctorID) ON UPDATE CASCADE ON DELETE SET NULL,
     FOREIGN KEY (scriptID) REFERENCES Prescriptions(scriptID) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
-# creating Message table
+-- creating Message table
 CREATE TABLE IF NOT EXISTS Message (
-    comID INT AUTO_INCREMENT PRIMARY KEY,
+    comID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     dateSent DATE,
-    subject VARCHAR(255),
+    [subject] VARCHAR(255),
     content TEXT,
     patientID INT,
     doctorID INT,
     coachID INT,
     repID INT,
-    deleted TINYINT(1) DEFAULT 0,
+    deleted TINYINT(1) DEFAULT 0 NOT NULL,
     FOREIGN KEY (patientID) REFERENCES Patient(patientID) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (doctorID) REFERENCES Doctor(doctorID) ON UPDATE CASCADE ON DELETE SET NULL,
     FOREIGN KEY (coachID) REFERENCES WellnessCoach(coachID) ON UPDATE CASCADE ON DELETE SET NULL,
     FOREIGN KEY (repID) REFERENCES InsuranceRepresentative(repID) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
-# creating InsurancePLan table
+-- creating InsurancePLan table
 CREATE TABLE IF NOT EXISTS InsurancePlan (
-    planID INT AUTO_INCREMENT PRIMARY KEY,
+    planID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     terminationDate DATE,
     copay DECIMAL,
-    description TEXT,
+    [description] TEXT,
     repID INT,
-    inactive TINYINT(1) DEFAULT 0,
+    inactive TINYINT(1) DEFAULT 0 NOT NULL,
     FOREIGN KEY (repID) REFERENCES InsuranceRepresentative(repID) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
-# creating BillingRecord table
+-- creating BillingRecord table
 CREATE TABLE IF NOT EXISTS BillingRecord (
-    billingRecordID INT AUTO_INCREMENT PRIMARY KEY,
-    description TEXT,
+    billingRecordID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    [description] TEXT,
     amount DECIMAL,
-    patientID INT,
+    patientID INT NOT NULL,
     planID INT,
-    paid TINYINT(1) DEFAULT 0,
+    paid TINYINT(1) DEFAULT 0 NOT NULL,
     FOREIGN KEY (patientID) REFERENCES Patient(patientID) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (planID) REFERENCES InsurancePlan(planID) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
-# creating WellnessRecord table
+-- creating WellnessRecord table
 CREATE TABLE IF NOT EXISTS WellnessRecord (
-    wellnessRecordID INT AUTO_INCREMENT PRIMARY KEY,
+    wellnessRecordID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     goal VARCHAR(255),
-    description TEXT,
-    patientID INT,
-    complete TINYINT(1) DEFAULT 0,
+    [description] TEXT,
+    patientID INT NOT NULL,
+    complete TINYINT(1) DEFAULT 0 NOT NULL,
     FOREIGN KEY (patientID) REFERENCES Patient(patientID) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
-# creating AllergyRecord table
+-- creating AllergyRecord table
 CREATE TABLE IF NOT EXISTS AllergyRecord (
-    healthRecordID INT AUTO_INCREMENT PRIMARY KEY,
+    healthRecordID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     allergies TEXT,
     FOREIGN KEY (healthRecordID) REFERENCES HealthRecords(healthRecordID) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-# creating vaxhistoryRecord table
+-- creating vaxhistoryRecord table
 CREATE TABLE IF NOT EXISTS vaxHistoryRecord (
-    healthRecordID INT AUTO_INCREMENT PRIMARY KEY,
+    healthRecordID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     vaxHistory TEXT,
     FOREIGN KEY (healthRecordID) REFERENCES HealthRecords(healthRecordID) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-# creating Notifications table
+-- creating Notifications table
 CREATE TABLE IF NOT EXISTS Notifications (
-    notificationID INT PRIMARY KEY,
+    notificationID INT PRIMARY KEY NOT NULL,
     content TEXT,
-    status VARCHAR(50),
+    [status] VARCHAR(50),
     timeSent TIMESTAMP,
-    patientID INT,
+    patientID INT NOT NULL,
     visitID INT,
     testID INT,
-    FOREIGN KEY (patientID) REFERENCES Patient(patientID) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (patientID) REFERENCES Patient
+
+(patientID) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (visitID) REFERENCES Visit(visitID) ON UPDATE CASCADE ON DELETE SET NULL,
     FOREIGN KEY (testID) REFERENCES LabResults(testID) ON UPDATE CASCADE ON DELETE SET NULL
 );
+
 
 # Drop all tables in reverse order to avoid foreign key constraint issues
 DROP TABLE IF EXISTS Notifications;
