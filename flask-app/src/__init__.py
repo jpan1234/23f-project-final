eb1432a1120df15edc0d38c3854474a897d99609
Jeffrey Pan <pan.je@northeastern.edu>
2023-12-04 17:04:11 -0500

    added user privileges

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -18,7 +18,7 @@ def create_app():
     app.config['MYSQL_DATABASE_USER'] = 'root'
     app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_root_password.txt').readline().strip()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
-    app.config['MYSQL_DATABASE_PORT'] = 3200
+    app.config['MYSQL_DATABASE_PORT'] = 3306
     app.config['MYSQL_DATABASE_DB'] = 'HuskyHealth'  # Change this to your DB name
 
     # Initialize the database object with the settings above. 
