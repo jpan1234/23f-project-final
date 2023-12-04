264a7b538e38ec8357780a397f45a0d5edbf0206
MasterChef444 <54949007+MasterChef444@users.noreply.github.com>
2023-12-04 15:52:42 -0500

    added file to store auto-generated data

---
+++ final project/generated_data.sql
@@ -0,0 +1,11 @@
+-- Patient data
+insert into Patient (patientid, firstname, lastname, birthdate, age) values (1, 'Henrie', 'Saywood', '11/28/2023', 5);
+insert into Patient (patientid, firstname, lastname, birthdate, age) values (2, 'Shamus', 'Nisuis', '8/11/2023', 6);
+insert into Patient (patientid, firstname, lastname, birthdate, age) values (3, 'Huey', 'Mougenel', '9/20/2023', 62);
+insert into Patient (patientid, firstname, lastname, birthdate, age) values (4, 'Victoir', 'Tattam', '2/21/2023', 39);
+insert into Patient (patientid, firstname, lastname, birthdate, age) values (5, 'Michaeline', 'Rosenschein', '9/1/2023', 86);
+insert into Patient (patientid, firstname, lastname, birthdate, age) values (6, 'Loy', 'Kluge', '8/7/2023', 53);
+insert into Patient (patientid, firstname, lastname, birthdate, age) values (7, 'Arnaldo', 'Embling', '7/2/2023', 53);
+insert into Patient (patientid, firstname, lastname, birthdate, age) values (8, 'Toni', 'Gaither', '7/25/2023', 88);
+insert into Patient (patientid, firstname, lastname, birthdate, age) values (9, 'Nanny', 'Tammadge', '8/30/2023', 30);
+insert into Patient (patientid, firstname, lastname, birthdate, age) values (10, 'Athena', 'Joselevitz', '7/3/2023', 7);
